9ada636dfd173a2be370034f5f3e316b7c2e80ae
Pete DeBiase <pete@debiase.io>
2022-01-23 06:32:56 -0800

    [LC121] Initial solution + faster alternate solution

---
+++ 121. Best Time to Buy and Sell Stock/best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+"""https://leetcode.com/problems/best-time-to-buy-and-sell-stock/"""
+
+import timeit
+
+class SolutionInitial:
+    # Time Complexity: O(n^2)
+    # Space Complexity: O(1)
+    def maxProfit(self, prices: list[int]) -> int:
+        max_profit = 0
+        for i in range(len(prices) - 1):
+            current_price = prices[i]
+            future_prices = prices[i + 1:]
+            max_future_price = max(future_prices)
+            if max_future_price > current_price:
+                max_profit_temp = max_future_price - current_price
+                max_profit = max_profit_temp if max_profit_temp > max_profit else max_profit
+        return max_profit
+
+class SolutionAlternate():
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
+    def maxProfit(self, prices: list[int]) -> int:
+        min_price, max_profit = float('inf'), 0
+        for price in prices:
+            min_price = price if price < min_price else min_price
+            profit_temp = price - min_price
+            max_profit = profit_temp if profit_temp > max_profit else max_profit
+        return max_profit
+
+
+if __name__ == '__main__':
+    solution_initial = SolutionInitial()
+    solution_alternate = SolutionAlternate()
+
+    # Example 1 (Expected Output: 5)
+    prices = [7, 1, 5, 3, 6, 4]
+    print(solution_initial.maxProfit(prices))
+    print(solution_alternate.maxProfit(prices))
+
+    # Example 2 (Expected Output: 0)
+    prices = [7, 6, 4, 3, 1]
+    print(solution_initial.maxProfit(prices))
+    print(solution_alternate.maxProfit(prices))
+
+    # Benchmarking
+    prices = list(range(1000))
+    print(timeit.timeit(lambda: solution_initial.maxProfit(prices), number=1000))
+    print(timeit.timeit(lambda: solution_alternate.maxProfit(prices), number=1000))
--- README.md
+++ README.md
@@ -13,6 +13,7 @@ Here is a link to my [LeetCode profile](https://leetcode.com/pete-debiase/)!
 
 - [1. Two Sum](#1-two-sum)
 - [48. Rotate Image](#48-rotate-image)
+- [121. Best Time to Buy and Sell Stock](#121-best-time-to-buy-and-sell-stock)
 - [217. Contains Duplicate](#217-contains-duplicate)
 
 <!-- /MarkdownTOC -->
@@ -31,6 +32,11 @@ Solutions that are O(n^2) and O(1) in time and space, respectively, can often be
 
 1. 2022-01-22 – Initial solution (Python)
 2. 2022-01-23 – Solution time = 3:48 (Python)
+
+#### [121. Best Time to Buy and Sell Stock](https://leetcode.com/problems/best-time-to-buy-and-sell-stock/)
+- `min()` and `max()` introduce quite a bit of overhead, even when called on a collection of just two items.
+
+1. 2022-01-23 – Initial solution (Python)
 
 #### [217. Contains Duplicate](https://leetcode.com/problems/contains-duplicate/)
 Real-world performance can be different from what Big-O notation says. Big-O notation is most applicable for "sufficiently large input", so if n is not sufficiently large, an algorithm with worse Big-O time complexity might actually outperform one with better Big-O time complexity.
