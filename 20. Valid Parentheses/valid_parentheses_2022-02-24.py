2d9ed1dd843db8daeab65cd37884c1db66e59ad2
Pete DeBiase <pete@debiase.io>
2022-02-24 04:23:32 -0800

    Daily review

---
+++ 20. Valid Parentheses/valid_parentheses_2022-02-24.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+"""https://leetcode.com/problems/valid-parentheses/"""
+
+class Solution:
+    def isValid(self, string: str) -> bool:
+        brackets = {'(': ')', '{': '}', '[': ']'}
+        stack = []
+        for c in string:
+            if c in brackets:
+                stack.append(c)
+            elif not stack or c != brackets[stack.pop()]:
+                return False
+        return not stack
+
--- README.md
+++ README.md
@@ -36,6 +36,7 @@ Solutions that are O(n^2) and O(1) in time and space, respectively, can often be
 #### [20. Valid Parentheses](https://leetcode.com/problems/valid-parentheses/)
 
 1. 2022-02-23 – Initial solution (Python)
+2. 2022-02-24 – Solution time = 3:25 (Python)
 
 
 #### [48. Rotate Image](https://leetcode.com/problems/rotate-image/)
