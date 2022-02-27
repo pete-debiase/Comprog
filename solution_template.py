853d6b9cc0852647b8832815ece5ee95ad85d599
Pete DeBiase <pete@debiase.io>
2022-02-27 00:51:24 -0800

    Add basic solution template

---
+++ solution_template.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+"""URL"""
+
+import timeit
+
+class SolutionInitial:
+    # Time Complexity:
+    # Space Complexity:
+    def function(self) -> None:
+        pass
+
+class SolutionPreferred:
+    # Time Complexity:
+    # Space Complexity:
+    def function(self) -> None:
+        pass
+
+if __name__ == '__main__':
+    solution_initial = SolutionInitial()
+    solution_preferred = SolutionPreferred()
+
+    # Example 1 (Expected Output: )
+    my_input = None
+    print(solution_initial.function(my_input))
+    print(solution_preferred.function(my_input))
+
+    # Benchmarking
+    number = 100
+    my_input = None
+    print(timeit.timeit(lambda: solution_initial.function(my_input), number=number))
+    print(timeit.timeit(lambda: solution_preferred.function(my_input), number=number))
