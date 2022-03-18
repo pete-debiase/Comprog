89430483068f8aae1c22cb65952dcb95c39eb9a3
Pete DeBiase <pete@debiase.io>
2022-03-17 22:53:25 -0700

    [LC242] Re-designate preferred solution

--- Solutions/242. Valid Anagram/valid_anagram.py
+++ Solutions/242. Valid Anagram/valid_anagram.py
@@ -4,7 +4,7 @@
 import timeit
 from collections import defaultdict, Counter
 
-class SolutionPreferred:
+class SolutionInitial:
     # Time / Space: O(n) / O(n)
     def isAnagram(self, s1: str, s2: str) -> bool:
         charmap_1 = self.string_to_charmap(s1)
@@ -20,13 +20,13 @@ class SolutionPreferred:
                 charmap[c] = 1
         return charmap
 
-class SolutionAlternate:
+class SolutionPreferred:
     # Time / Space: O(n) / O(n)
     def isAnagram(self, s1: str, s2: str) -> bool:
         charmap = defaultdict(int)
         for c in s1: charmap[c] += 1
         for c in s2: charmap[c] -= 1
-        return all(_ == 0 for _ in charmap.values())
+        return all(v == 0 for v in charmap.values())
 
 class SolutionAlternate2:
     # Time / Space: O(n) / O(n)
@@ -35,24 +35,24 @@ class SolutionAlternate2:
 
 
 if __name__ == '__main__':
+    solution_initial = SolutionInitial()
     solution_preferred = SolutionPreferred()
-    solution_alternate = SolutionAlternate()
     solution_alternate2 = SolutionAlternate2()
 
     # Example 1 (Expected Output: True)
     s1, s2 = 'anagram', 'nagaram'
+    print(solution_initial.isAnagram(s1, s2))
     print(solution_preferred.isAnagram(s1, s2))
-    print(solution_alternate.isAnagram(s1, s2))
     print(solution_alternate2.isAnagram(s1, s2))
 
     # Example 2 (Expected Output: False)
     s1, s2 = 'rat', 'car'
+    print(solution_initial.isAnagram(s1, s2))
     print(solution_preferred.isAnagram(s1, s2))
-    print(solution_alternate.isAnagram(s1, s2))
     print(solution_alternate2.isAnagram(s1, s2))
 
     # Benchmarking
     s1, s2 = 'anagram', 'nagaram'
-    print(timeit.timeit(lambda: solution_preferred.isAnagram(s1, s2), number=100_000))
-    print(timeit.timeit(lambda: solution_alternate.isAnagram(s1, s2), number=100_000)) # Slower
+    print(timeit.timeit(lambda: solution_initial.isAnagram(s1, s2), number=100_000))
+    print(timeit.timeit(lambda: solution_preferred.isAnagram(s1, s2), number=100_000)) # Slower
     print(timeit.timeit(lambda: solution_alternate2.isAnagram(s1, s2), number=100_000)) # Slower
