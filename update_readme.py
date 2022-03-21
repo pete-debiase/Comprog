6d738b00f8521693f11aed0ab5f96c8f829146cb
Pete DeBiase <pete@debiase.io>
2022-03-20 22:35:38 -0700

    Split README updater script into functions
    Was starting to get too unwieldy to work on/debug due to namespace pollution.

--- update_readme.py
+++ update_readme.py
@@ -2,82 +2,55 @@
 """Update main README"""
 
 import os
-import re
 from airium import Airium
 
-# ┌─────────────────────────────────────────────────────────────────────────────
-# │ Build sorted dictionary of solution metadata
-# └─────────────────────────────────────────────────────────────────────────────
-solutions_root, solutions = r'Solutions/', {}
-for root, dirs, files in os.walk(solutions_root):
-    if root == solutions_root: continue # Skip first (parent) entry from os.walk
 
-    problem_title = os.path.basename(root)
-    problem_id, title_text = problem_title.split('. ')
-    anchor = f"#{problem_id}-{title_text.lower().replace(' ', '-')}"
+def build_solution_metadata_dict():
+    """Build a sorted dictionary of solution metadata."""
+    solutions_root, solutions = r'Solutions/', {}
+    for root, dirs, files in os.walk(solutions_root):
+        if root == solutions_root: continue # Skip first (parent) entry from os.walk
 
-    problem = {'root': root,
-                    'title': problem_title,
-                    'anchor': anchor,
-                    'files': files}
-    solutions[int(problem_id)] = problem
+        problem_title = os.path.basename(root)
+        problem_id, title_text = problem_title.split('. ')
+        anchor = f"#{problem_id}-{title_text.lower().replace(' ', '-')}"
 
-solutions = {k: solutions[k] for k in sorted(solutions)}
+        problem = {'root': root,
+                        'title': problem_title,
+                        'anchor': anchor,
+                        'files': files}
+        solutions[int(problem_id)] = problem
 
-# ┌─────────────────────────────────────────────────────────────────────────────
-# │ Build HTML problem table
-# └─────────────────────────────────────────────────────────────────────────────
-num_columns = 15
-links = [f'''<a href="{solutions[k]['anchor']}">{k}</a>''' for k in solutions]
-links_chunked = [links[i:i + num_columns] for i in range(0, len(links), num_columns)]
-links_chunked[-1] += [''] * (num_columns - len(links_chunked[-1])) # Pad last chunk if necessary
+    solutions = {k: solutions[k] for k in sorted(solutions)}
+    return solutions
 
-a = Airium()
-with a.table():
-    for chunk in links_chunked:
-        with a.tr():
-            [a.td(align='center', _t=link) for link in chunk]
-table = str(a)
 
-# Insert HTML problem table into README
-with open('README.md', 'r', encoding='utf-8') as file:
-    readme = file.read()
+def generate_html_problem_table(solutions):
+    """Generate HTML problem table."""
+    num_columns = 15
+    links = [f'''<a href="{solutions[k]['anchor']}">{k}</a>''' for k in solutions]
+    links_chunked = [links[i:i + num_columns] for i in range(0, len(links), num_columns)]
+    links_chunked[-1] += [''] * (num_columns - len(links_chunked[-1])) # Pad last chunk if necessary
 
-marker = '<!-- Auto-generated table -->'
-readme = readme.split(marker)
-readme[1] = f'{marker}\n{table}\n{marker}'
+    a = Airium()
+    with a.table():
+        for chunk in links_chunked:
+            with a.tr():
+                [a.td(align='center', _t=link) for link in chunk]
+    table = str(a)
 
-with open('README.md', 'w+', newline='\n', encoding='utf-8') as file:
-    file.write(''.join(readme))
+    # Insert HTML problem table into README
+    with open('README.md', 'r', encoding='utf-8') as file:
+        readme = file.read()
 
-# ┌─────────────────────────────────────────────────────────────────────────────
-# │ READMEs
-# └─────────────────────────────────────────────────────────────────────────────
-with open('README.md', 'r', encoding='utf-8') as file:
-    readme_main = file.read()
+    marker = '<!-- Auto-generated table -->'
+    readme = readme.split(marker)
+    readme[1] = f'{marker}\n{table}\n{marker}'
 
-subreadmes = readme_main.split('####')[1:]
-subreadmes = [_.strip() + '\n' for _ in subreadmes]
+    with open('README.md', 'w+', newline='\n', encoding='utf-8') as file:
+        file.write(''.join(readme))
 
-for k, subreadme in zip(solutions, subreadmes):
-    root = solutions[k]['root']
-    filename = root + '/README.md'
 
-    problem_title = solutions[k]['title'].replace(' ', '%20')
-    solution_files = solutions[k]['files']
-    URL_base = f'https://github.com/pete-debiase/Comprog/blob/main/Solutions/{problem_title}/'
-    solution_URLs = [URL_base + file for file in solution_files if '.py' in file]
-    solution_URLs = [f'| [Python]({URL}) |' for URL in solution_URLs]
-
-    full_title = f"### [{solutions[k]['title']}](URL) ([solutions]({URL_base}))"
-
-    readme = f"""{full_title}\nNOTES\n\nTABLE\n\n{solution_URLs}"""
-
-    subreadme = subreadme.split('\n')
-    subreadme[0] = f"### {subreadme[0]} ([solutions]({URL_base}))"
-    subreadme = '\n'.join(subreadme)
-
-    subreadme = re.sub(r'\|(\s*Python\s*)\|', lambda match: str(solution_URLs.pop(0)), subreadme)
-
-    with open(filename, 'w+', newline='\n', encoding='utf-8') as file:
-        file.write(subreadme)
+if __name__ == '__main__':
+    solutions = build_solution_metadata_dict()
+    generate_html_problem_table(solutions)
